1a928ce1f2eac1cdde628d05596e1db845a7fa63
Summer <summerkatherine1@gmail.com>
2017-08-28 20:18:41 -0400

    refactored

---
+++ src/main/java/babysitter/BabySitter.java
@@ -0,0 +1,23 @@
+package babysitter;
+
+public class BabySitter {
+
+	public Boolean startTime(int startTime) {
+			return startTime >= 17;
+	}
+
+	public Boolean endTime(int endTime) {
+		return endTime <= 4;
+	}
+
+	public Boolean bedTime(int bedTime) {
+		return bedTime == 20;
+	}
+
+	public int startToBedTimePayRate(int pay) {
+		return 12;
+	}
+
+
+
+}
---
+++ src/test/java/babysitter/BabySitterTest.java
@@ -0,0 +1,52 @@
+package babysitter;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BabySitterTest {
+
+	// Act
+	BabySitter babySitter = new BabySitter();
+
+	@Test
+	public void startsAtFivePm() {
+		Boolean number = babySitter.startTime(17);
+		// Assert
+		assertEquals(true, number);
+	}
+
+	@Test
+	public void endsAtFourAm() {
+		Boolean number = babySitter.endTime(4);
+		// Assert
+		assertEquals(true, number);
+	}
+
+	@Test
+	public void startsAfterFivePm() {
+		Boolean number = babySitter.startTime(18);
+		// Assert
+		assertEquals(true, number);
+	}
+
+	@Test
+	public void endsBeforeFourAm() {
+		Boolean number = babySitter.endTime(3);
+		// Assert
+		assertEquals(true, number);
+	}
+	
+	@Test
+	public void bedtimeAtEight() {
+		Boolean number = babySitter.bedTime(20);
+		assertEquals(true, number);
+	}
+
+	@Test
+	public void shouldGetPaidFromStartToBedTime() {
+     int pay = babySitter.startToBedTimePayRate(startsAtFivePm() , bedtimeAtEight());
+     assertEquals(12, pay);
+	}
+
+}
